77c2ae0c4e5303a5996a68973d1e06580c4c0140
Mihael Vrbanec <miqster@gmx.de>
2010-05-26 22:27:24 +0200

    add stub for pattern matching examples

---
+++ src/de/miq/knowledge/patternmatch/PatternMatcher.scala
@@ -0,0 +1,9 @@
+package de.miq.knowledge.patternmatch
+
+
+object PatternMatcher {
+
+  def main(args: Array[String]) {
+    // TODO show examples for the different ways of pattern matching in Scala 
+  }
+}
