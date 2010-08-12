5d2dc2813610a3dc9444dfe7c3ea677c1e6c685d
Mihael Vrbanec <miqster@gmx.de>
2010-08-12 21:51:49 +0200

    demonstrate pattern matching with extraction of method return values

--- src/de/miq/knowledge/patternmatch/PatternMatcher.scala
+++ src/de/miq/knowledge/patternmatch/PatternMatcher.scala
@@ -1,9 +1,25 @@
 package de.miq.knowledge.patternmatch
 
+import caseclasses.Name
+
 
 object PatternMatcher {
 
+  def getYearMonthTuple : (Int, String) = (2010, "February")
+
+  def getName : Name = new Name("John", "Doe")
+
+  def extraction: Unit = {
+    val (year, month) = getYearMonthTuple
+    println("year:" + year)
+    println("month:" + month)
+    val Name(f, s) = getName
+    println("Combined name: " + s + ", " + f)
+    println("Full name: " + getName.toString)
+  }
+
   def main(args: Array[String]) {
-    // TODO show examples for the different ways of pattern matching in Scala 
+    // TODO show examples for the different ways of pattern matching in Scala
+    extraction
   }
 }
